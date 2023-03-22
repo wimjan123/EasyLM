47316fc92e69e84cdd94d246ce52c4d2b7b18530
wimjan123 <visser.william@live.nl>
2023-03-22 20:55:11 +0100

    Update export_checkpoint.py

--- EasyLM/scripts/export_checkpoint.py
+++ EasyLM/scripts/export_checkpoint.py
@@ -18,13 +18,16 @@ FLAGS, FLAGS_DEF = mlxu.define_flags_with_default(
     output=('', 'output Flax msgpack file'),
 )
 
-
 def main(argv):
     assert FLAGS.input != '' and FLAGS.output != '', 'input and output must be specified'
-    params = StreamingCheckpointer.load_checkpoint(FLAGS.input)['params']['params']
+
+    loaded_checkpoint = StreamingCheckpointer.load_checkpoint(FLAGS.input)
+    print("Keys in the loaded checkpoint:")
+    pprint.pprint(loaded_checkpoint.keys())
+
+    params = loaded_checkpoint['params']  # Access the parameters directly
     with mlxu.open_file(FLAGS.output, 'wb') as fout:
         fout.write(flax.serialization.msgpack_serialize(params, in_place=True))
-
 
 if __name__ == "__main__":
     mlxu.run(main)
