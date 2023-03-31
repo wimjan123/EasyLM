8b60b3daeb6a5fa4045796e21ce3ef90772cd584
wimjan123 <visser.william@live.nl>
2023-03-31 23:43:23 +0200

    2

--- execute.sh
+++ execute.sh
@@ -1,3 +1,9 @@
+export TPU_NAME=gpt-4
+export ZONE=us-central2-b
+export RUNTIME_VERSION=tpu-vm-v4-base
+gcloud compute tpus tpu-vm ssh ${TPU_NAME} --zone=${ZONE} --worker=0
+
+
 gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
 --zone=${ZONE} \
 --worker=all --command='git clone https://github.com/wimjan123/EasyLM.git && cd EasyLM/ && export PYTHONPATH="${PWD}:$PYTHONPATH" && sh ./scripts/tpu_vm_setup.sh'
@@ -6,3 +12,23 @@ gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
 gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
 --zone=${ZONE} \
 --worker=all --command='cd EasyLM/ && python3 -m EasyLM.models.gptj.gptj_train --train_dataset.type='huggingface''
+
+gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
+--zone=${ZONE} \
+--worker=all --command='cd EasyLM/ && python3 -m EasyLM.models.llama.llama_train --train_dataset.type='huggingface''
+
+gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
+--zone=${ZONE} \
+--worker=all --command='kill -9 21200'
+
+gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
+--zone=${ZONE} \
+--worker=all --command=' cd EasyLM/ && git pull'
+
+
+
+
+gcloud compute tpus tpu-vm ssh ${TPU_NAME} \
+--zone=${ZONE} \
+--worker=all \
+--command='cd EasyLM/ && python3 -m EasyLM.models.gptj.gptj_train --train_dataset.type='huggingface' --logger.online True --logger.gcs_output_dir gs://gpt-j-train/GPT-4-JAX'
