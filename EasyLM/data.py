a6ee5a5dbf89a85a6413e6d96dbc4f5d92c654d1
wimjan123 <visser.william@live.nl>
2023-04-01 13:25:07 +0200

    Update data.py

--- EasyLM/data.py
+++ EasyLM/data.py
@@ -120,7 +120,7 @@ class HuggingfaceDataset(object):
         config.split = 'train'
         config.streaming = False
         config.seq_length = 2048
-        config.batch_size = 16
+        config.batch_size = 8
 
         if updates is not None:
             config.update(ConfigDict(updates).copy_and_resolve_references())
