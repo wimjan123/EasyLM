d0b07a31e946852dfe948d6932d8d8aa174ee43d
wimjan123 <visser.william@live.nl>
2023-04-03 19:06:59 +0200

    f

--- execute.sh
+++ execute.sh
@@ -42,3 +42,20 @@ python3 -m EasyLM.models.gptj.gptj_train --train_dataset.type='huggingface' --lo
 python3 -m EasyLM.models.llama.llama_train --train_dataset.type='huggingface' --logger.online True --logger.gcs_output_dir gs://gpt-j-train/llama4-output/ --optimizer.type=adamw --optimizer.accumulate_gradient_steps=32
 
 python3 -m EasyLM.models.llama.llama_train --train_dataset.type='huggingface' --logger.online True --logger.gcs_output_dir gs://gpt-j-train/llama4-output/ --optimizer.type=adamw --optimizer.accumulate_gradient_steps=32
+
+
+python3 -m EasyLM.models.llama.llama_train \
+--mp_mesh_dim='16,1' \
+--total_steps='250000' \
+--load_llama_config='13b' \
+--load_checkpoint='params::/llama/13B/streaming_params' \
+--initialize_jax_distributed='True' \
+--save_model_freq='1000' \
+--save_milestone_freq='4000' \
+--log_freq='1000' \
+--optimizer.type=adamw \
+--optimizer.accumulate_gradient_steps=32 \
+--eval_steps='0' \
+--logger.gcs_output_dir='gs://gpt-j-train/GPT-4/' \
+--logger.online='True' \
+--tokenizer.vocab_file='/llama/tokenizer.model'
