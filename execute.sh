45193ab48fff09e275248b62c44e009721464ccd
wimjan123 <visser.william@live.nl>
2023-04-07 22:57:25 +0200

    vcb

--- execute.sh
+++ execute.sh
@@ -51,17 +51,17 @@ python3 -m EasyLM.models.llama.llama_train \
 --checkpointer.float_dtype='fp32' \
 --load_checkpoint='params::gs://gpt-j-train/llama_stream/13B/streaming_params' \
 --initialize_jax_distributed='True' \
---save_model_freq='5000' \
+--save_model_freq='500' \
 --save_milestone_freq='10000' \
 --log_freq='500' \
 --optimizer.type=adamw \
 --optimizer.adamw_optimizer.lr=1e-4 \
---bf16_accumulate_gradient='True' \
- -optimizer.accumulate_gradient_steps=32 \
+--optimizer.accumulate_gradient_steps=32 \
 --eval_steps='0' \
 --logger.gcs_output_dir='gs://gpt-j-train/GPT-4/' \
 --logger.online='True' \
---tokenizer.vocab_file='/llama/tokenizer.model' 
+--tokenizer.vocab_file='/llama/tokenizer.model' \
+--log_all_worker='True'
 
 
 python -m EasyLM.scripts.convert_checkpoint \
@@ -71,10 +71,10 @@ python -m EasyLM.scripts.convert_checkpoint \
 
 
 python3 -m EasyLM.scripts.convert_checkpoint \
-    --load_checkpoint='params::gs://gpt-j-train/GPT-4/36db7e39151446a0be9539dac57f4833/streaming_params' \
+    --load_checkpoint='params::gs://gpt-j-train/llama_stream/13B/streaming_params' \
     --output_file='gs://gpt-j-train/GPT-4/36db7e39151446a0be9539dac57f4833/flax-model.msgpack' \
     --float_dtype='fp32' \
-    --streaming FALSE
+    --streaming=False
 
 
 
@@ -98,7 +98,7 @@ python3 -m EasyLM.scripts.convert_checkpoint \
 python3 -m EasyLM.models.llama.llama_serve \
     --mp_mesh_dim='-1,1' \
     --load_llama_config='13b' \
-    --load_checkpoint='params::gs://gpt-j-train/llama_stream/13B/streaming_params' \
+    --load_checkpoint='params::gs://gpt-j-train/GPT-4/c3f79936779d4de3a88e73fd32140931/streaming_params' \
     --dtype='bf16' \
     --lm_server.host='0.0.0.0' \
     --lm_server.pre_compile='loglikelihood' \
@@ -106,7 +106,29 @@ python3 -m EasyLM.models.llama.llama_serve \
     --seq_length=2048 \
     --tokenizer.vocab_file='/llama/tokenizer.model' 
 
+ 10000
+110000
 
 
-
-
+python3 -m EasyLM.models.llama.llama_train \
+--mp_mesh_dim='16,1' \
+--total_steps='110000' \
+--load_llama_config='13b' \
+--checkpointer.float_dtype='fp32' \
+--load_checkpoint='params::gs://gpt-j-train/llama_stream/13B/streaming_params' \
+--initialize_jax_distributed='True' \
+--save_model_freq='5000' \
+--save_milestone_freq='10000' \
+--log_freq='500' \
+--optimizer.type=adamw \
+--optimizer.adamw_optimizer.lr=5e-5 \
+--optimizer.adamw_optimizer.end_lr=1e-5 \
+--optimizer.adamw_optimizer.lr_warmup_steps='10000' \
+--optimizer.adamw_optimizer.lr_decay_steps='100000' \
+--optimizer.accumulate_gradient_steps=32 \
+--optimizer.bf16_accumulate_gradient='True' \
+--eval_steps='0' \
+--logger.gcs_output_dir='gs://gpt-j-train/GPT-4/' \
+--logger.online='True' \
+--tokenizer.vocab_file='/llama/tokenizer.model' \
+--log_all_worker='True'
